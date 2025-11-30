b68bb7622eff4025366f0810f2e33b71550cc202
sabbir hossain <mdsabbir477470@gmail.com>
2025-11-30 15:35:23 +0600

    example added

--- 4variable.php
+++ 4variable.php
@@ -1,2 +1,13 @@
 <?php
-echo "";
+$name = "Sabbir Hossain Dev";
+$age = 24;
+$level = 4;
+ 
+// echo $name;
+// echo "-";
+// echo $age;
+
+
+echo "I am " . $name . " I specialize at Full Stack | PHP Leravel | WebFlow | WordPress Developer💠 I have " . $level ." Year's experience."; // . use concat string + er jonno.
+echo PHP_EOL;
+echo "My name is $name. I am $age year's old. I specialize at Full Stack | PHP Leravel | WebFlow | WordPress Developer💠";
