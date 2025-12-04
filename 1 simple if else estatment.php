7a42951380dc0875eee33253bbe9dc5a5f0ecd90
sabbir hossain <mdsabbir477470@gmail.com>
2025-12-04 08:43:30 +0600

    if else estatment add

---
+++ 1 simple if else estatment.php
@@ -0,0 +1,12 @@
+<?php
+$currentTime = "";
+
+if($currentTime == "morning"){
+    echo "Good Morning";
+} else if($currentTime == "evening"){
+    echo "Good Evening";
+} else if($currentTime == "afternoon"){
+    echo "Good Afternoon";
+} else{
+    echo "Good Night"; // answer
+}
