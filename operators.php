f9cb6143129f4f52f45fa44c99e40c501eaf14cc
sabbir hossain <mdsabbir477470@gmail.com>
2025-12-04 13:56:55 +0600

    operaters also logic add

--- operators.php
+++ operators.php
@@ -9,3 +9,14 @@ if($x > $y){
 } else{
     echo "$x is same as $y";
 }
+
+echo PHP_EOL;
+
+$person1 = "Sabbir Hossain";
+$person2 = "Sabbir";
+
+if($person1 != $person2){
+    echo "They are not same person";
+} else{
+    echo "They are same person";
+}
