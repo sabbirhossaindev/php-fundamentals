cae91cf64489167c2cf197852b14821a1eb766ad
sabbir hossain <mdsabbir477470@gmail.com>
2025-12-05 12:12:25 +0600

    for loop and while loop logic add

--- 1 for loop.php
+++ 1 for loop.php
@@ -4,3 +4,14 @@ for($i = 1; $i <= 10; $i++){
     echo "$i = Hello Sabbir";
     echo PHP_EOL;
 }
+
+echo PHP_EOL;
+
+// print even number 2-100
+for ($i = 2; $i <= 100; $i += 2){
+    $remainder = $i % 2;
+    if ($remainder == 0){
+        echo "The Even Number is = $i";
+        echo PHP_EOL;
+    }
+}
---
+++ 2 while loop.php
@@ -0,0 +1,12 @@
+<?php
+$i = 2;
+while($i <= 100){
+    echo $i;
+    echo PHP_EOL;
+    $i += 2;
+}
+
+// first add a number
+// second while (condition)
+// third print output
+// forth condition koto gor kore cholbe.
