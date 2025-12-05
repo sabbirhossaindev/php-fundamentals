e0c4a193970ba0c3df36110026f0754f9730c16e
sabbir hossain <mdsabbir477470@gmail.com>
2025-12-05 11:53:46 +0600

    match logic add

--- 4 multiple condition.php
+++ 4 multiple condition.php
@@ -8,9 +8,19 @@ switch (true){
     case ($age >= 14 && $age <= 20);
     echo "Teenager";
     break;
-    case ($age >= 20 && $age < 60);
+    case ($age >= 21 && $age < 60);
     echo "Adult";
     break;
     default:
     echo "Senior";
 }
+
+echo PHP_EOL;
+
+// using match expression (PHP 8.0+);
+echo match(true) {
+    $age <= 13 => "child",
+    $age >= 14 && $age <= 20 => "Teenager",
+    $age >= 21 && $age <= 60 => "Adult",
+    default => "Senior",
+};
