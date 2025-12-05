a3b66ee2cee72583dcb1015add879a36a3c4ba96
sabbir hossain <mdsabbir477470@gmail.com>
2025-12-05 19:12:04 +0600

    array add

---
+++ 1 array.php
@@ -0,0 +1,8 @@
+<?php
+$numbers = array (1, 2, 3, 7, 6, 8, 10); // o indexed
+$moreNumbers = [12, 34, 77, 88, 43];
+
+print_r($numbers);             
+echo  $numbers[3]; // thats means position ta check korlo 7 shoe hobe
+echo PHP_EOL;
+echo $moreNumbers[4];                                                     
